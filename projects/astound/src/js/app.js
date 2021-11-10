0c1f663d23ab5158deb267b7e37cf86bada95ce9
Denys Fesenko <dfesuk@gmail.com>
2021-11-10 10:28:25 +0200

    finish. but without js

--- projects/astound/src/js/app.js
+++ projects/astound/src/js/app.js
@@ -7,31 +7,3 @@ const prevBtn = document.querySelector('#prevBtn'),
 
 let counter = 1;
 const size = carouserItem[0].clientWidth;
-
-// carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
-
-prevBtn.addEventListener('click', ()=> {
-  if(counter <= 0)
-  carouselSlide.style.transition = "transform 0.4s ease-in-out";
-  counter--;
-  carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)'
-})
-
-nextBtn.addEventListener('click', ()=> {
-  carouselSlide.style.transition = "transform 0.4s ease-in-out";
-  counter++;
-  carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
-  if(carouserItem[counter].id === carouserItem.length){
-    carouselSlide.style.transform = "none";
-    // counter = carouserItem.length - 2;
-    // carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)' 
-  }
-})
-
-carouselSlide.addEventListener('transitionend', () =>{
-  if(carouserItem[counter].id === carouserItem.length){
-    carouselSlide.style.transition = "none";
-    // counter = carouserItem.length - 2;
-    // carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)' 
-  }
-})
